bfb227bffe6325252ab5dda799b712515a613753
alpha02 <you@example.com>
2024-09-03 22:55:23 +0700

    fix home page

--- src/components/LatestProject.jsx
+++ src/components/LatestProject.jsx
@@ -12,7 +12,9 @@ const LatestProject = ({ image, title, desc, reverse }) => {
       </div>
       <div className="text-center md:text-left w-[70%]">
         <h2 className="text-base mb-2">{title}</h2>
-        <p className="text-sm sm:text-base">{desc}</p>
+        <p className=" line-clamp-3 md:line-clamp-none text-sm sm:text-base">
+          {desc}
+        </p>
         <button className="bg-transparent border border-pink-400 text-pink-400 mt-4 px-6 py-2 rounded hover:bg-pink-400 hover:text-white transition-colors">
           View Project
         </button>
